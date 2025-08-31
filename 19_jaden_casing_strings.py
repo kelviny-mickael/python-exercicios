aff2b1b5e4f729e12b8a87462d6d4c42ddf093bd
kelviny-mickael <kelvinymickael48@gmail.com>
2025-08-31 18:04:25 -0300

    first commit

---
+++ 19_jaden_casing_strings.py
@@ -0,0 +1,25 @@
+# Jaden Smith, filho de Will Smith, é o astro de filmes como Karatê Kid (2010) e 
+# Depois da Terra (2013). Jaden também é conhecido por algumas de suas filosofias, 
+# que ele transmite via Twitter. Ao escrever no Twitter, ele é conhecido 
+# por quase sempre usar letras maiúsculas em todas as palavras. 
+# Para simplificar, você terá que usar letras maiúsculas em cada palavra. 
+# Veja como as contrações devem ser feitas no exemplo abaixo.
+# Sua tarefa é converter strings para como seriam escritas por Jaden Smith. 
+# As strings são citações reais de Jaden Smith, mas não são escritas 
+# com maiúsculas da mesma forma que ele as digitou originalmente.
+# Exemplo:
+# Not Jaden-Cased: "How can mirrors be real if our eyes aren't real".
+# Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real".
+
+def to_jaden_case(str):
+    words_list = str.split()
+    new_list_word = []
+
+    for word in words_list:
+        capitalize_each_word = word.capitalize()
+    
+        new_list_word.append(capitalize_each_word)
+
+    return ' '.join(new_list_word)
+
+print(to_jaden_case('How can mirrors be real if our eyes aren\'t real'))
